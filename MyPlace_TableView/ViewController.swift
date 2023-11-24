9450361f3a768ed438afaa523465552067dad6aa
Sergey <sergro1974@gmail.com>
2023-11-24 21:43:58 +0800

    Initial Commit

---
+++ MyPlace_TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlace_TableView
+//
+//  Created by Сергей Громов on 24.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
