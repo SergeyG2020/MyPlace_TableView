91c8d00f3ef64d2b6645cc4e00d3bf55f165d881
Sergey <sergro1974@gmail.com>
2023-11-25 02:48:10 +0800

    Initial 2

--- MyPlace_TableView/ViewController.swift
+++ MyPlace_TableView/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let restaurant = ["Taiga", "Turist", "Шашлыкоф"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - UITableViewDelegate, UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurant.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurant[indexPath.row]
+        cell.backgroundColor = UIColor.clear
+        cell.imageView?.image = UIImage(named: "car1")
+        cell.focusStyle = .custom
+        cell.isEditing = true
+        cell.setEditing(true, animated: true)
+   
+        return cell
+    }
+    
 
 }
 
